1cc6521441e77107630f4574732afce1c09d6173
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-05-12 16:02:52 -0300

    Adicionar arquivos de projeto.

---
+++ desenvolvimento backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace desenvolvimento_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
